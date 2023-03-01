b115af0a647ce31a82a5c16be83881b1314e6a90
Bagrat Dabaghyan <dbagrat@gmail.com>
2023-03-01 11:41:19 +0400

    Migrate to Conan 2.0

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,6 @@
-from conans import ConanFile, tools
+from conan import ConanFile
+from conan.tools import files
+
 import os
 
 class FastNoiseLiteConan(ConanFile):
@@ -9,24 +11,22 @@ class FastNoiseLiteConan(ConanFile):
     homepage = "https://github.com/Auburn/FastNoiseLite"
     description = "FastNoise Lite is an extremely portable open source noise generation library with a large selection of noise algorithms"
     topics = ("noise", "perlin", "simplex")
+    package_type = "header-library"
     no_copy_source = True
-
-    @property
-    def _dir_name(self):
-        return "Cpp"
 
     @property
     def _source_commit(self):
         return "6be3d6bf7fb408de341285f9ee8a29b67fd953f1"
 
-    @property
-    def _source_subfolder(self):
-        return os.path.join(self.source_folder, self._dir_name)
-
     def source(self):
-        tools.get(url="https://github.com/Auburn/FastNoiseLite/archive/{}.zip".format(self._source_commit),
-                  pattern="*/{}/*".format(self._dir_name),
+        files.get(self,
+                  url="https://github.com/Auburn/FastNoiseLite/archive/{}.zip".format(self._source_commit),
+                  pattern="*/Cpp/*",
                   strip_root=True)
 
     def package(self):
-        self.copy("*.h", dst="include", src=self._source_subfolder, keep_path=False)
+        files.copy(self,
+                   pattern="*.h",
+                   src=self.source_folder,
+                   dst=os.path.join(self.package_folder, "include"),
+                   keep_path=False)
